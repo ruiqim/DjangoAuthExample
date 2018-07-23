278343667a7497c7a868048b3232d069f88c8940
rmao4 <rmao4@ford.com>
2018-07-23 14:37:34 -0400

    created authentication models.py and serializers.py from base template

---
+++ conduit/apps/authentication/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Users
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """Serializers registration requests and creates a new user."""
+
+    # Ensure passwords are at least 8 characters long, no longer than 128
+    # characters, and can not be read by the clientself.
+
+    password = serialziers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    # The clieant should not be able to send a token along with a registration
+    # request. making `token` read-only and handles that for us.
+    token = serialziers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        # List all of the fields that could possibly be included in a request
+        # or response, including fields specified explicitly above.
+
+        fields = ['email', 'username', 'password', 'token']
+
+    def create(self, validated_data):
+        # Use the `create_user` method we wrote earlier to create a new user
+        return User.objects.create_user(**validated_data)
--- conduit/settings.py
+++ conduit/settings.py
@@ -45,7 +45,16 @@ INSTALLED_APPS = [
     'conduit.apps.authentication',
     'conduit.apps.core',
     'conduit.apps.profiles',
+
+    
 ]
+
+# Tell Django about the custom `User` model we created. The string
+# `authentication.User` tells Django we are referring to the `User` model in
+# the `authentication` module. This module is registered above in a setting
+# called `INSTALLED_APPS`.
+AUTH_USER_MODEL = 'authentication.User'
+
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
