2ce7f04925714f795e31e25c275ce9276b5f036d
rmao4 <rmao4@ford.com>
2018-07-24 13:28:09 -0400

    completed JWT auth

--- conduit/settings.py
+++ conduit/settings.py
@@ -73,6 +73,9 @@ ROOT_URLCONF = 'conduit.urls'
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'conduit.apps.core.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'conduit.apps.authentication.backends.JWTAuthentication',
+    ),
 }
 
 TEMPLATES = [
